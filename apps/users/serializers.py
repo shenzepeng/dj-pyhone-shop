5a698957b1912d1cfea7d33b1f5349c360fa2fb2
mac <806871609@qq.com>
2021-04-28 20:30:47 +0800

    inti

---
+++ apps/users/serializers.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+from rest_framework.validators import UniqueValidator
+import re
+from datetime import datetime, timedelta
+from DaishuShop.settings import REGEX_MOBILE
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class SmsSerializer(serializers.Serializer):
+    mobile = serializers.CharField(max_length=11)
+
+    def validate_mobile(self, mobile):
+        """
+        验证手机号码(函数名称必须为validate_ + 字段名)
+        """
+        # 手机是否注册
+        if User.objects.filter(mobile=mobile).count():
+            raise serializers.ValidationError("用户已经存在")
+
+        # 验证手机号码是否合法
+        if not re.match(REGEX_MOBILE, mobile):
+            raise serializers.ValidationError("手机号码非法")
+
+        return mobile
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    """
+    用户详情序列化
+    """
+
+    class Meta:
+        model = User
+        fields = ("username", "gender", "birthday", "email", "mobile")
+
+class UserRegSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(label="用户名", help_text="用户名", required=True, allow_blank=False,
+                                     validators=[UniqueValidator(queryset=User.objects.all(), message="用户已经存在")])
+
+    password = serializers.CharField(
+        style={'input_type': 'password'}, help_text="密码", label="密码", write_only=True,
+    )
+
+    # 调用父类的create方法，该方法会返回当前model的实例化对象即user。
+    # 前面是将父类原有的create进行执行，后面是加入自己的逻辑
+    def create(self, validated_data):
+        user = super(UserRegSerializer, self).create(validated_data=validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+    # 不加字段名的验证器作用于所有字段之上。attrs是字段 validate之后返回的总的dict
+    def validate(self, attrs):
+        attrs["mobile"] = attrs["username"]
+        return attrs
+
+    class Meta:
+        model = User
+        fields = ("username", "mobile", "password")
